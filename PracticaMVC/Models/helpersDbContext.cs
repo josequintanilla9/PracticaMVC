f4d4399273bb8bdb1beb1147c9c476cd631c91f5
Jose Quintanilla <josequintanillarodriguez513@gmail.com>
2024-04-05 21:27:49 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/helpersDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+	public class helpersDbContext : DbContext
+	{
+		public helpersDbContext(DbContextOptions  options) : base(options)
+		{
+		}
+
+		public DbSet<usuario> usuario { get; set; }
+	}
+}
---
+++ PracticaMVC/Models/usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+	public class usuario
+	{
+		[Key]
+		public int id_usuario { get; set; }
+		public string? nombre_usuario { get; set; }
+		public string? password { get; set; }
+		public string? genero { get; set; }
+		public string? direccion { get; set; }
+		public string? pasatiempos { get; set; }
+		public string? cursos { get; set; }
+		public string? conocimientos { get; set; }
+	}
+}
